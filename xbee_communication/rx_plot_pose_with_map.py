459da9d4d3c935fe40868cd0a391f2aa4b7b1427
MengGuo <guomeng3@gmail.com>
2017-01-25 22:43:37 -0500

    translation fixed

--- xbee_communication/rx_plot_pose_with_map.py
+++ xbee_communication/rx_plot_pose_with_map.py
@@ -18,17 +18,28 @@ import re
 def norm(pose1, pose2):
     return sqrt((pose1[0]-pose2[0])**2+(pose1[1]-pose2[1])**2)
 
-def transpose(pose, initial_pose, initial_pose_in_map):
-    dif_pose = [pose[i]-initial_pose[i] for i in range(0,3)]
-    
+def rotate(vec, theta):
+    new_x = -vec[0]*sin(theta) - vec[1]*cos(theta)
+    new_y = vec[0]*cos(theta) - vec[1]*sin(theta)
+    return [new_x, new_y]
+
+def transpose(pose, init_pose, init_pose_in_map):
+    dif_pose = [pose[i]-init_pose[i] for i in range(0,3)]
+    # reflection in y-axis
+    dif_pose[1] = -dif_pose[1]
+    dif_pose[2] = -dif_pose[2]
+    dif_init_theta = init_pose_in_map[2]+init_pose[2]
+    dif_pose_in_map = rotate(dif_pose, dif_init_theta-1.57)
+    pose_in_map = [init_pose_in_map[i]+dif_pose_in_map[i] for i in range(0,2)]
+    theta_in_map = pose[2]+init_pose_in_map[2]-init_pose[2]
+    pose_in_map.append(3.14-atan2(sin(theta_in_map),cos(theta_in_map)))
     return pose_in_map
 
 def transform(car, center, dl):
     new_car = []
     for node in car:
         vec = [node[0]-center[0], node[1]-center[1]]
-        new_x = vec[0]*cos(dl) - vec[1]*sin(dl)
-        new_y = vec[0]*sin(dl) + vec[1]*cos(dl)
+        [new_x, new_y] = rotate(vec, dl)
         new_car.append([new_x+center[0], new_y+center[1]])
     return new_car
 
@@ -47,14 +58,9 @@ def visualize_jackal(figure, pose, img):
         L1 = 0.8*meter_to_pixel
         L2 = 1.6*meter_to_pixel
         car=[(xl-L1,yl-L1), (xl-L1,yl+ L1), (xl, yl+L2), (xl+L1, yl+L1), (xl+L1,yl-L1)]
-        polygon2 = Polygon(transform(car, [xl,yl], dl), fill = True, facecolor='blue', edgecolor='blue', lw=4, zorder=2)
+        polygon2 = Polygon(transform(car, [xl,yl],dl+3.14), fill = True, facecolor='blue', edgecolor='blue', lw=4, zorder=2)
         ax.add_patch(polygon2)    
     ax.grid()
-    # ax.set_xlabel('x(m)')
-    # ax.set_ylabel('y(m)')
-    # ax.set_aspect('equal')
-    # ax.set_xlim(-10, 10)
-    # ax.set_ylim(-10, 10)
     #fig.subplots_adjust(0.003,0.062,0.97,0.94)
     #pyplot.show()
     pyplot.pause(0.01)
@@ -90,6 +96,7 @@ while True:
             pose = (float(data[0]), float(data[1]), float(data[2]))
             print 'new pose %s' %str(pose)
     pose_in_map = transpose(pose, initial_pose, initial_pose_in_map)
-    figure = visualize_jackal(figure, pose, img)
+    print 'pose in map %s' %str(pose_in_map)
+    figure = visualize_jackal(figure, pose_in_map, img)
 
 ser.close()
